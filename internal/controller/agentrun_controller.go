d478cd2f89e5caa4ed40d89264acc32a1d92ef2f
SamuZad <47607001+SamuZad@users.noreply.github.com>
2026-05-07 07:51:28 +0100

    emit secretKey instead of tokenEnv in MCP servers config (#163)

--- internal/controller/agentrun_controller.go
+++ internal/controller/agentrun_controller.go
@@ -3828,8 +3828,8 @@ type mcpServerYAML struct {
 }
 
 type mcpAuthYAML struct {
-	Type     string `yaml:"type"`
-	TokenEnv string `yaml:"tokenEnv"`
+	Type      string `yaml:"type"`
+	SecretKey string `yaml:"secretKey"`
 }
 
 type mcpServersConfigYAML struct {
@@ -3862,8 +3862,8 @@ func buildMCPServersYAML(mcpServers []sympoziumv1alpha1.MCPServerRef) (string, e
 		if srv.AuthSecret != "" {
 			envName := fmt.Sprintf("MCP_AUTH_%s", strings.ToUpper(strings.ReplaceAll(srv.Name, "-", "_")))
 			entry.Auth = &mcpAuthYAML{
-				Type:     "bearer",
-				TokenEnv: envName,
+				Type:      "bearer",
+				SecretKey: envName,
 			}
 		}
 
